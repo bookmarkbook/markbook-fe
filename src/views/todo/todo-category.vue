9370e1ea32135d52b3e86b4d056fd2049a79e286
MikiAlex <18516340862@163.com>
2018-08-11 13:22:41 +0800

    try view motion

--- src/views/todo/todo-category.vue
+++ src/views/todo/todo-category.vue
@@ -1,39 +1,122 @@
 <template>
   <div class="todo-category">
     <h2 class="title">{{title}}</h2>
+
+    <Task
+      v-for="item in items"
+      :key="item.id"
+      :id = "item.id"
+      :title="item.title"
+      :animation="item.view"
+      @mousedown="handelMouseDown"
+    />
+
   </div>
 </template>
 
 <script>
+import { Motion } from "vue-motion";
+import Task from "./todo-task.vue";
+
 export default {
-  props:{
-    title:{
-      default: 'title'
+  props: {
+    title: {
+      default: "title"
     }
+  },
+  components: { Motion, Task },
+  mounted() {
+    // setTimeout(()=>{
+    //   this.sizes;
+    //   console.log('sdsfd');
+    // }, 3000)
+  },
+  computed: {
+    mixedView() {
+      return this.items.map(item => item);
+    }
+  },
+  methods: {
+    setOnesView(newviewPartial, id) {
+      this.items = this.items.map(item => {
+        if (item.id === id) {
+          return this.setView({ scale: 2 }, item);
+        }
+        return item
+      });
+      console.log(this.items);
+    },
+    setView(newviewPartial, olditem) {
+      return {
+        ...olditem,
+        view: {
+          ...olditem.view,
+          ...newviewPartial
+        }
+      };
+    },
+    handelMouseDown(param) {
+      this.setOnesView({ scale: 2 }, param.id );
+    },
+    handleMouseUp(param) {}
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          title: "sdfsdf sdfsdf",
+          view: {
+            shadowSize: 10,
+            x: 0,
+            y: 0,
+            scale: 1
+          }
+        },
+        {
+          id: 2,
+          title: "sdfsdf sdfsdfsdf",
+          view: {
+            shadowSize: 10,
+            x: 0,
+            y: 0,
+            scale: 1
+          }
+        },
+        {
+          id: 3,
+          title: "sdfsdf sdfsdfsdf",
+          view: {
+            shadowSize: 10,
+            x: 0,
+            y: 0,
+            scale: 1
+          }
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-.todo-category{
+.todo-category {
   width: 200px;
   height: 400px;
   background: #fbfbfb;
-  position:relative;
+  position: relative;
 
-  box-shadow:
-  0 2px 4px 0 rgba(0,0,0,0.09), 
-  0 -2px 0 0 rgba(255,255,255,0.50), 
-  0 8px 15px 0 rgba(0,0,0,0.04);
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.09),
+    0 -2px 0 0 rgba(255, 255, 255, 0.5), 0 8px 15px 0 rgba(0, 0, 0, 0.04);
   border-radius: 5px;
 }
 
-.title{
+.title {
   font-size: 24px;
-  color:#d7d7d7;
+  color: #d7d7d7;
   position: absolute;
-  top:-55px;
-  right:0px;
+  top: -55px;
+  right: 0px;
 }
 </style>
 
--- src/views/todo/todo-task.vue
+++ src/views/todo/todo-task.vue
@@ -1,26 +1,64 @@
 <template>
-  <div>
-    {{content}}
-  </div>
+  <Motion tag="div" :values="animation">
+    <template slot-scope="ani">
+      <div class="todo-item"
+      @mousedown="handelMouseDown"
+      @mouseup="handelMouseUp"
+      :style="{
+          boxShadow: `rgba(0, 0, 0, 0.2) 0px ${ani.shadow}px ${2 * ani.shadow}px 0px`,
+          transform: `translate3d(${ani.x}px, ${ani.y}px, 0) scale(${ani.scale})`
+          }"
+      >
+        {{title}}
+      </div>
+    </template >
+  </Motion>
 </template>
 
 <script>
+import { Motion } from "vue-motion";
 export default {
-  props:{
-    editable:{
-      default:false,
+  components: { Motion },
+  methods: {
+    handelMouseDown() {
+      this.$emit("mousedown", {
+        id: this.id
+      });
     },
-    content:{
-      default: 'a task'
+    handelMouseUp() {
+      this.$emit("mouseup", {
+        id: this.id
+      });
     },
-    description:{
-      default: 'a task description'
+  },
+  props: {
+    editable: {
+      default: false
+    },
+    id: {
+      default: 0
+    },
+    title: {
+      default: "a task"
+    },
+    description: {
+      default: "a task description"
+    },
+    animation: {
+      require: true
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
+.todo-item {
+  width: 100%;
+  height: 50px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  background: #f5f5f5;
+  cursor: pointer;
+}
 </style>
 
