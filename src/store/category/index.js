182d96e206eff50f2b28b5fd162ceaeb2e8cdc29
MikiAlex <18516340862@163.com>
2018-08-25 22:34:26 +0800

    dev

---
+++ src/store/category/index.js
@@ -0,0 +1,16 @@
+export default {
+  namespaced: true,
+  state: {
+    tree: {
+      name: 'cate',
+      children: [
+        {
+          name: 'cate2',
+        },
+        {
+          name: 'cate3',
+        }
+      ]
+    }
+  }
+}
---
+++ src/views/bookmark/category/tree-part.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="item">
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    data:{
+      require:true
+    }
+  }
+}
+</script>
+
+
+<style>
+.item{
+  border-top:1px solid #eee;
+}
+</style>
